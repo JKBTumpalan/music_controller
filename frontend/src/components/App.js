7eada75b82362887791a5d778f6ee20e3082d2a8
JKBTumpalan <jbtumpalan@up.edu.ph>
2022-07-04 21:25:32 +0800

    Added and integrated react

---
+++ frontend/src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+export const App = () => {
+    return (
+        <h1> Testing React Code! </h1>
+    )
+}
+
+const appDiv = document.getElementById("app");
+render(<App/>, appDiv)
